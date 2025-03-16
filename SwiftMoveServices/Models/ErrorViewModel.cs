f6198ec4afc79c2838ec94782a2079d001b8c0d1
Darkrager7 <123085155+Darkrager7@users.noreply.github.com>
2025-03-16 12:20:45 +0000

    Add project files.

---
+++ SwiftMoveServices/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SwiftMoveServices.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
